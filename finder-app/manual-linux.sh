387814678ffae7eee91d9b1644c4fb8fbea16da1
aysvaryag <aysvaryag@Ubuntu.myguest.virtualbox.org>
2025-02-03 02:41:33 -0700

    not working

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
+#REFERENCES:https://askubuntu.com/questions/1376461/copy-all-from-a-folder-into-the-project-root-directory
 
 set -e
 set -u
@@ -33,11 +34,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    #  kernel building 
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper #deep clean the kernel build tree
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig #configure our virtual dev board
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all # build kernel image for booting
+#make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules #build modules
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs #build device tree
 
-    # TODO: Add your kernel build steps here
 fi
 
 echo "Adding the Image in outdir"
+cp -r ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image  ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,7 +54,13 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Creating necessary base directories
+echo " root file system"
+mkdir ${OUTDIR}/rootfs  #root files system
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,12 +68,20 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Configuring  busybox
+    make distclean
+    make defconfig
+
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+echo "installing busy box"
+#installing busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
@@ -68,13 +89,50 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 
-# TODO: Make device nodes
+export SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
 
-# TODO: Clean and build the writer utility
+sudo cp -L $SYSROOT/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+sudo cp -L $SYSROOT/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+sudo cp -L $SYSROOT/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+sudo cp -L $SYSROOT/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+
+
+cd "${OUTDIR}/rootfs"
+#device nodes
+sudo rm -f /dev/null
+sudo rm -f /dev/console
+
+sudo mknod -m 666 "/dev/null" c 1 3 # null device
+sudo mknod -m 600 "/dev/console" c 5 1 # console device 
+
+# Clean and build the writer utility
+cd ${FINDER_APP_DIR} 
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} #make with arm compiler
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -a ./writer ${OUTDIR}/rootfs/home  # -a preserves attributes
+cp -a ./finder.sh ${OUTDIR}/rootfs/home 
+cp -r ./conf/ ${OUTDIR}/rootfs/home #/. copies hidden files 
+cp -a ./finder-test.sh ${OUTDIR}/rootfs/home
+cp -a ./autorun-qemu.sh  ${OUTDIR}/rootfs/home
+cp -a ./writer.c ${OUTDIR}/rootfs/home
+cp -a ./Makefile  ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
 
-# TODO: Create initramfs.cpio.gz
+#Chown the root directory
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root * #make contents owned by root
+
+# Create initramfs.cpio.gz
+#cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ..
+#cd ${OUTDIR}
+gzip -f initramfs.cpio
+
+
+
+
